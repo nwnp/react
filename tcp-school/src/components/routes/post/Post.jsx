8a8659b2634a0397244e5a8a7ed55a5c2f1e016e
mtbjin <jjw_0418@naver.com>
2023-07-10 11:48:43 +0900

    routing test

--- tcp-school/src/components/routes/post/Post.jsx
+++ tcp-school/src/components/routes/post/Post.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-export const Post = () => {
+export const Post = (props) => {
+  const { dataLength, title, description } = props;
+  console.log(dataLength, title, description)
+  const posts = () => {
+    const arr = [];
+    for (let i = 0; i < dataLength; i++) {
+      arr.push(
+        <div className="post" key={i}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      );
+    }
+    return arr;
+  }
+
   return (
-    <div>
-      Post List
-    </div>
+    <div>{posts()}</div>
   );
 };
--- tcp-school/src/components/routes/post/PostIndex.jsx
+++ tcp-school/src/components/routes/post/PostIndex.jsx
@@ -6,7 +6,13 @@ import { PostCreate } from './PostCreate';
 export const PostIndex = () => {
   return (
     <Routes>
-      <Route path="/posts" element={<Post />}></Route>
+      <Route 
+        path="/posts"
+        dataLength="6"
+        title="test title"
+        description="test description"
+        element={<Post />}
+      ></Route>
       <Route path="/post/create" element={<PostCreate />}></Route>
     </Routes>
   );
