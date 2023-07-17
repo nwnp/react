c99ecacc60b83f9bdb48dff6244e11a7804b878f
진정우 <jin@jinjeong-uui-MacBookPro.local>
2023-07-18 01:31:53 +0900

    git name test

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -4,6 +4,7 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="game">
+      <p>test</p>
       <div className="game-board"></div>
       <Board />
       <div className="game-info"></div>
