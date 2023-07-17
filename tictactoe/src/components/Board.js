05eb35498be89d87a9b48198ddf62b0791b53379
진정우 <jin@jinjeong-uui-MacBookPro.local>
2023-07-18 00:36:32 +0900

    tictactoe app ended

--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -3,8 +3,26 @@ import Square from "./Square";
 import "./Board.css";
 
 export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSqaure(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares });
   }
 
   render() {
--- tictactoe/src/components/Square.js
+++ tictactoe/src/components/Square.js
@@ -3,19 +3,10 @@ import React from "react";
 import "./Square.css";
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
-    const clicked = () => this.setState({ value: "X" });
-
     return (
-      <button className="square" onClick={clicked}>
-        {this.state.value == null ? this.props.value : this.state.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
