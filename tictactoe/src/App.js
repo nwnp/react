0e1196dad4711ecd55f2114a093eb09dcb4130a7
nwnp <jjw_0418@naver.com>
2023-07-18 01:36:47 +0900

    git name test

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -4,7 +4,6 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="game">
-      <p>test</p>
       <div className="game-board"></div>
       <Board />
       <div className="game-info"></div>
