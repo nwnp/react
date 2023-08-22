7b88b3f697902aad824639b7c725aea3b492d7a7
nwnp <jjw_0418@naver.com>
2023-08-22 21:45:31 +0900

    make a ToDo List using Redux

--- redux-learn/src/App.tsx
+++ redux-learn/src/App.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import { RootState } from "./reducers";
 
 type PropsType = {
   value: any;
@@ -7,11 +10,39 @@ type PropsType = {
 };
 
 function App({ value, onIncrement, onDecrement }: PropsType) {
+  /* variables */
+  const [todoValue, setTodoValue] = useState("");
+  // useSelector는 state를 가져온다.
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+  // useDispatch는 dispatch를 가져온다.
+  const dispatch = useDispatch();
+
+  /* functions */
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
+    setTodoValue("");
+  };
+
   return (
     <div className="App">
-      {/* Clicked: {value} times */}
+      Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit"></input>
+      </form>
     </div>
   );
 }
