c872150be4ebc975f5b6e4d0ed245bc63ad1cfd1
nwnp <jjw_0418@naver.com>
2023-08-23 20:28:25 +0900

    practice redux-thunk

--- redux-learn/src/App.tsx
+++ redux-learn/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import { RootState } from "./reducers";
+import { fetchPosts } from "./actions/posts";
 
 type PropsType = {
   value: any;
@@ -9,14 +10,25 @@ type PropsType = {
   onDecrement: () => void;
 };
 
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+}
+
 function App({ value, onIncrement, onDecrement }: PropsType) {
   /* variables */
   const [todoValue, setTodoValue] = useState("");
   // useSelector는 state를 가져온다.
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
   // useDispatch는 dispatch를 가져온다.
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts()); // action은 객체이어야 하는데 함수를 dispatch 하기 때문에 error가 발생한다.
+  }, [dispatch]);
 
   /* functions */
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,6 +55,11 @@ function App({ value, onIncrement, onDecrement }: PropsType) {
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit"></input>
       </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- redux-learn/src/index.tsx
+++ redux-learn/src/index.tsx
@@ -3,15 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const store = createStore(rootReducer);
+// Middleware
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("dispatching");
+  next(action);
+};
+
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+const store = createStore(rootReducer, middleware);
 
 store.dispatch({
   type: "ADD_TODO",
