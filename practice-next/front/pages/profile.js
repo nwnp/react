ef596cb5b83b85b738c74dfabfa8758b3034f6ae
nwnp <jjw_0418@naver.com>
2023-09-18 21:16:53 +0900

    profile page, components

--- practice-next/front/pages/profile.js
+++ practice-next/front/pages/profile.js
@@ -1,14 +1,30 @@
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+import FollowerList from "../components/FollowerList";
 
 const profile = () => {
+  const followerList = [
+    { nickname: "제로초" },
+    { nickname: "바보" },
+    { nickname: "노드버드오피셜" },
+  ];
+  const followingList = [
+    { nickname: "제로초" },
+    { nickname: "바보" },
+    { nickname: "노드버드오피셜" },
+  ];
+
   return (
     <>
       <AppLayout>
         <Head>
           <title>내 프로필 | NodeBird</title>
         </Head>
-        <div>프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowerList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
