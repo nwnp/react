08f0902219d90e7dd228f106269ee9737eb3370c
nwnp <jjw_0418@naver.com>
2023-10-30 20:50:20 +0900

    useMemo, useCallback을 사용한 컴포넌트 성능 최적화

--- velopert/todo-app/src/App.jsx
+++ velopert/todo-app/src/App.jsx
@@ -3,47 +3,47 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import { useCallback, useRef, useState } from 'react';
 
+const createBulTodos = () => {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+
+  return array;
+};
+
 const App = () => {
   // variables
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
-    { id: 3, text: '리액트의 일정 관리 앱 만들어보기', checked: false },
-  ]);
+  const [todos, setTodos] = useState(createBulTodos);
 
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
 
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
