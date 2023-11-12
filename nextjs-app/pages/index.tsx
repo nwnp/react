5033b557621419cd22553efd7c9c5e5e087ebd39
nwnp <jjw_0418@naver.com>
2023-11-12 17:01:41 +0900

    next settings

---
+++ nextjs-app/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import homeStyles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>J2W</title>
+      </Head>
+      <section className={homeStyles.headingMd}>
+        <p>[J2W Introduction]</p>
+        <p>(This is a website)</p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}></ul>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
